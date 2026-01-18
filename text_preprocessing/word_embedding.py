b901f2f2226a8398d4ce1dbd3b797851a6aac995
Wiwi Huang <kobeskskk@gmail.com>
2026-01-18 16:22:05 +0800

    Finish word_embedding.py

--- text_preprocessing/word_embedding.py
+++ text_preprocessing/word_embedding.py
@@ -5,6 +5,11 @@ Embedding layer
 """
 from .sliding_window_with_torch import Build_GPTDataLoader
 import torch.nn as nn
+import torch
+
+'''
+Build a dataloader to generate input-target pairs
+'''
 
 txt = "just for test"
 max_length = 4
@@ -19,6 +24,9 @@ inputs, targets = next(data_iter)
 # print(inputs)  # temporary output to verify the dataloader
 # print(inputs.shape)
 
+'''
+Build a token embedding layer
+'''
 vocab_size = 50257  # GPT-2 vocabulary size
 output_dim = 256
 token_embedding_layer = nn.Embedding(
@@ -28,3 +36,22 @@ token_embedding_layer = nn.Embedding(
 token_embeddings = token_embedding_layer(inputs)
 
 # print(token_embeddings.shape)  # temporary output to verify the embedding layer
+
+'''
+Build a position embedding layer
+'''
+
+pos_embedding_layer = nn.Embedding(
+    num_embeddings= max_length,
+    embedding_dim= output_dim,
+)
+pos_embeddings = pos_embedding_layer(torch.arange(max_length))
+
+
+'''
+input_embedding = token_embedding + position_embedding
+'''
+
+input_embeddings = token_embeddings + pos_embeddings
+
+# print(input_embeddings.shape)  # temporary output to verify the final input embeddings

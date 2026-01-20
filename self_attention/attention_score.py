f7c02deec257c9a9e2ec3cc257d0f3e81fd3a850
Wiwi Huang <kobeskskk@gmail.com>
2026-01-20 13:15:36 +0800

    Implement scale dot-product attention score

---
+++ self_attention/attention_score.py
@@ -0,0 +1,53 @@
+'''
+A word has three projected representations in a sentence:
+
+- Query: how this word seeks relevant information
+- Key: how this word is represented for matching
+- Value: the information this word provides if attended to
+
+Therefore, it is beneficial to use different projections
+for Query, Key, and Value.
+'''
+
+import torch
+
+inputs = torch.tensor(
+    [
+        [0.43, 0.15, 0.89],
+        [0.55, 0.87, 0.66],
+        [0.57, 0.85, 0.64],
+        [0.22, 0.58, 0.33],
+        [0.77, 0.25, 0.10],
+        [0.05, 0.80, 0.55],
+    ]
+)
+
+'''
+Establishing the query, key, value matrices 
+'''
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+torch.manual_seed(123)  # For reproducibility
+
+# W_q, W_k, W_v are projection matrices for whole input but not only for one word.
+W_q = torch.nn.Parameter(torch.rand(d_in, d_out))  # nn.Parameter makes it learnable
+W_k = torch.nn.Parameter(torch.rand(d_in, d_out))
+W_v = torch.nn.Parameter(torch.rand(d_in, d_out))
+
+'''
+Computing the attention score for the second word
+'''
+
+query_2 = x_2 @ W_q
+# print(query_2)  # DEBUG: temporary output to verify the query vector
+
+keys = inputs @ W_k
+values = inputs @ W_v
+
+attn_scores_2 = query_2 @ keys.T
+
+d_k = keys.shape[1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim= 0)  # Scale dot-product attention

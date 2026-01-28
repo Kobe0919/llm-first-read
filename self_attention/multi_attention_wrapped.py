3329226b0f7c0abe100719652b7d4f86a2245bab
Wiwi Huang <kobeskskk@gmail.com>
2026-01-28 13:36:06 +0800

    feat: implement multi_attention_with_causal_masking

---
+++ self_attention/multi_attention_wrapped.py
@@ -0,0 +1,14 @@
+import torch.nn as nn 
+from causal_attention import CausalAttention
+import torch
+
+class MultiHeadAttention_wrapped(nn.Module):
+    def __init__(self, d_in, d_out, dropout, context_length, num_heads):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(d_in= d_in, d_out= d_out, dropout=dropout, context_length=context_length) 
+             for _ in range(num_heads)]
+        )
+    
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim= -1)
